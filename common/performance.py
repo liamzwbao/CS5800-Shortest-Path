5310a44c40ce941df443dd86e0232df925f50fae
Annabelle Sun <49429865+AnnabelleAB@users.noreply.github.com>
2023-04-24 22:41:35 -0700

    update performance file

--- common/performance.py
+++ common/performance.py
@@ -8,6 +8,68 @@ from common.graph import get_airports, get_flights, Graph, get_first_n_airports,
     get_flights_limited_airports, Edge
 
 
+def draw_line_graph_same_datapoint():
+    vertices = get_first_n_airports(100)
+    edges = get_flights_limited_airports(vertices)
+
+    # Convert the Edge objects to use Vertex objects as the src and dst attributes
+    edges = [Edge(vertices[edge.src.val], vertices[edge.dst.val], edge.weight) for edge in
+             edges]
+
+    graph = Graph()
+    graph.build_graph(vertices.values(), edges)
+
+    iterations = list(range(1, 101))
+    dijkstra_heap_times = []
+    dijkstra_unsorted_times = []
+    astar_heap_times = []
+    astar_unsorted_times = []
+    floyd_warshall_times = []
+
+    # Run Floyd-Warshall once and store the result
+    floyd_warshall_result = floyd_warshall(vertices, edges)
+
+    # Run algorithms and measure time
+    for _ in iterations:
+        src, dst = random.choice(list(vertices.keys())), random.choice(
+            list(vertices.keys()))
+
+        start = time.time()
+        dijkstra_heap(src, dst, vertices, graph)
+        dijkstra_heap_times.append(time.time() - start)
+
+        start = time.time()
+        dijkstra_unsorted_list(src, dst, vertices, graph)
+        dijkstra_unsorted_times.append(time.time() - start)
+
+        start = time.time()
+        astar_heap(src, dst, vertices, graph)
+        astar_heap_times.append(time.time() - start)
+
+        start = time.time()
+        astar_unsorted_list(src, dst, vertices, graph)
+        astar_unsorted_times.append(time.time() - start)
+
+        # Measure the time taken to access the distance from the precomputed result
+        start = time.time()
+        _ = floyd_warshall_result[src][dst]
+        floyd_warshall_times.append(time.time() - start)
+
+    # Plot results
+    plt.plot(iterations, dijkstra_heap_times, label="Dijkstra Heap")
+    plt.plot(iterations, dijkstra_unsorted_times, label="Dijkstra Unsorted List")
+    plt.plot(iterations, astar_heap_times, label="A* Heap")
+    plt.plot(iterations, astar_unsorted_times, label="A* Unsorted List")
+    plt.plot(iterations, floyd_warshall_times, label="Floyd-Warshall")
+
+    plt.xlabel("Iterations")
+    plt.ylabel("Elapsed Time (s)")
+    plt.title("Algorithms Performance Comparison")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
 def draw_line_graph():
     datapoints = list(range(100, 1001, 100))
     dijkstra_heap_avg_times = []
@@ -83,4 +145,5 @@ def draw_line_graph():
 
 
 if __name__ == "__main__":
+    draw_line_graph_same_datapoint()
     draw_line_graph()
